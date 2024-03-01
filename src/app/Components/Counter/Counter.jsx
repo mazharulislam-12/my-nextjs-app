ec090699c404dd9d126cd08052847bcca8ad85fb
Mazharul Islam <505mazharulislam@gmail.com>
2024-03-01 21:14:15 +0600

    clinet side commponent

--- src/app/Components/Counter/Counter.jsx
+++ src/app/Components/Counter/Counter.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
 
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h2>Counter {counter}</h2>
+
+      <button
+        className="btn btn-primary mr-2"
+        onClick={() => setCounter(counter + 1)}
+      >
+        Increase
+      </button>
+      <button
+        className="btn btn-primary"
+        onClick={() => setCounter(counter - 1)}
+      >
+        Decrease
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,11 @@
-"use client"
-import React, { useState } from 'react';
+
+import Counter from "./Components/Counter/Counter.jsx";
 
 const HomePage = () => {
-  const [counter, setCounter] = useState(0);
 
   return (
     <div>
-      <h2>This is Next Js App</h2>
-
-      <h2>Counter {counter}</h2>
-
-      <button className='btn btn-primary mr-2' onClick={() => setCounter(counter + 1)}>
-        Increase
-      </button>
-      <button className='btn btn-primary' onClick={() => setCounter(counter - 1)}>
-        Decrease
-      </button>
+      <Counter></Counter>
     </div>
   );
 };
