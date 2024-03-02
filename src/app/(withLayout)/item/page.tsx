f0118735c17412096811ab6fdd92e1d8ca51af05
Mazharul Islam <505mazharulislam@gmail.com>
2024-03-02 12:05:13 +0600

    grouping route

---
+++ src/app/(withLayout)/item/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemPage = () => {
+    return (
+        <div>
+            <h2>This is item page...</h2>
+        </div>
+    );
+};
+
+export default ItemPage;
