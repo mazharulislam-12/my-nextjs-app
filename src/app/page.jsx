7ac0995d2446ef4dd841004044399274321688c0
Mazharul Islam <505mazharulislam@gmail.com>
2024-03-02 12:20:21 +0600

    link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,17 @@
 
+import Link from "next/link.js";
 import Counter from "./Components/Counter/Counter.jsx";
 
 const HomePage = () => {
   return (
     <div>
       <Counter></Counter>
+      <Link href={`/about`}>
+        <button className="btn btn-info">About</button>
+      </Link>
+      <Link href={`/news`}>
+        <button className="btn btn-info">News</button>
+      </Link>
     </div>
   );
 };
